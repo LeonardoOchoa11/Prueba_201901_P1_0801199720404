8897d39c42fccf36e102821d7a904f430b47ad66
Leonardo Ochoa <leo8a11@hotmail.com>
2019-02-07 16:45:06 -0600

    Prueba Final y Coleccion

--- pruebabkend/routes/api/things.js
+++ pruebabkend/routes/api/things.js
@@ -1,14 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var fileModel = require('./pruebamodel');
+var data = null;
+
+var bitThingTp = {
+    'Empresa' : '',
+    'url': '',
+    'Nombre': '',
+    'Year': '',
+    'Rating': '',
+    'fechaIng': ''
+}
 
 router.get('/', function(req, res, next){
-    res.json({"msg":"ok"});
+    if(!data){
+        fileModel.read(function(err, filedata){
+            if(err){
+                console.log(err);
+                data = [];
+                return res.status(500).json({'error': 'Error al obtener Data'});
+            }
+            data = JSON.parse(filedata);
+            return res.status(200).json(data);
+        });
+    } else{
+            return res.status(200).json(data);
+
+    }    
 }); // get
 
 router.post('/new', function(req, res, next){
-    var _thingsData = req.body;
-    console.log(_thingsData);
-    res.json({"msg":"ok"});
+    
+    var _thingsData = Object.assign({}, bitThingTp, req.body);
+    if(!data){
+        data = [];
+    }
+    data.push(_thingsData);
+    fileModel.write(data, function(err){
+        if(err){ 
+            console.log(err);
+            return res.status(500),json({'error': 'Error al obtener Data'});
+        }
+        return res.status(200).json(_thingsData);
+    })
+    
 }); //nuevo thing
 
 router.put('/done/:thingId', function(req, res, next){
